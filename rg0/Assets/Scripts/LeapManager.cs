d002296e27312db138d1651d5cfd95942e8e5738
Cypress Frankenfeld <iamcypress@gmail.com>
2013-09-06 13:01:53 -0400

    Added sphere that moves with the pointer position in x, y coordinates.

---
+++ rg0/Assets/Scripts/LeapManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapManager : MonoBehaviour
+{
+    public PointerController m_pointerController;
+
+    private Controller m_controller;
+
+    void Start ()
+    {
+        m_controller = new Controller();
+        m_controller.EnableGesture(Gesture.GestureType.TYPECIRCLE,true);
+        m_controller.EnableGesture(Gesture.GestureType.TYPESWIPE,true);
+    }
+
+    void Update ()
+    {
+        Frame frame = m_controller.Frame();
+
+        // update the pointer controller
+        ToolList tools = frame.Tools;
+        if (tools.Count > 0)
+        {
+            Tool tool = tools[0];
+            Vector3 positionVector = new Vector3(tool.TipPosition.x, tool.TipPosition.y, tool.TipPosition.z);
+            m_pointerController.ToolMoved(positionVector);
+        }
+
+        // do something with the tracking data in the frame...
+        GestureList gestures = frame.Gestures();
+        if (gestures.Count > 0)
+        {
+            foreach (Gesture gesture in gestures)
+            {
+                if (gesture.State == Gesture.GestureState.STATESTOP)
+                {
+                    Debug.Log(gesture.Type + " : " + gesture.Duration);
+                }
+            }
+        }
+    }
+}
---
+++ rg0/Assets/Scripts/PointerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerController : MonoBehaviour
+{
+
+	public float m_movementScale;
+	public Vector3 m_positionOffset;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	// When the LeapManager detects that the leap has moved,
+	// it calls ToolMoved
+	public void ToolMoved(Vector3 position)
+	{
+		// we don't want to worry about where it is in the z axis
+		position.z = 0;
+		transform.position = (position * m_movementScale) + m_positionOffset;
+	}
+}
