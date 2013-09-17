b52540be3cdd482d63d751960f44e6f210e6d746
Cypress Frankenfeld <iamcypress@gmail.com>
2013-09-16 23:27:48 -0400

    we now save local copy of network instantiated player

--- rg0/Assets/Scripts/Server.cs
+++ rg0/Assets/Scripts/Server.cs
@@ -54,13 +54,13 @@ public class Server : MonoBehaviour
 		// if there's no server, let's make one
 		if (MasterServer.PollHostList().Length == 0)
 		{
-			m_playerIndex = 0;
 			Debug.Log("There's no host");
 			Network.InitializeServer(1, 5000, true);
 			MasterServer.RegisterHost("SpellGame", "Game Instance");
 
-			m_players[m_playerIndex] = CreatePlayer(m_initialServerLocation, m_initialServerRotation);
-			m_networkView.RPC("ConfigurePlayer", RPCMode.AllBuffered, m_playerIndex);
+			m_playerIndex = 0;
+			NetworkViewID viewID = Network.AllocateViewID();
+			m_networkView.RPC("CreatePlayer", RPCMode.AllBuffered, m_playerIndex, viewID);
 		}
 
 		// otherwise, let's connect to the first server in the list
@@ -74,28 +74,44 @@ public class Server : MonoBehaviour
 	}
 
 	[RPC]
-	GameObject ConfigurePlayer (int playerIndex)
+	GameObject CreatePlayer (int playerIndex, NetworkViewID viewID)
 	{
-		GameObject player = m_players[playerIndex];
+		// set the position based on the player index
+		Vector3 initialLocation = playerIndex == 1 ? m_initialServerLocation : m_initialClientLocation;
+		Quaternion initialRotation = playerIndex == 1 ? m_initialServerRotation : m_initialClientRotation;
+
+		// create the player object
+		GameObject player = Resources.Load("Player") as GameObject;
+		player = Instantiate(player, initialLocation, initialRotation) as GameObject;
+
+		// get the network view id from the player
+		NetworkView networkView = player.GetComponent<NetworkView>(); 
+		networkView.viewID = viewID;
+
+		// if the player is mine, connect it to the leap controller
+		if (viewID.isMine)
+		{
+			m_leapManager.ConnectPlayer(player);
+		}
+
+		// save a local reference to the player
+		m_players[playerIndex] = player;
+
+		// set the color and the name
 		VisualizationController visualizationController = player.GetComponent<VisualizationController>();
 		visualizationController.InitializeColors(m_playerColors[playerIndex]);
 		player.name = playerIndex == 1 ? "HostPlayer" : "ClientPlayer";
+
 		return player;
 	}
 
-	GameObject CreatePlayer (Vector3 initialLocation, Quaternion initialRotation)
-	{
-		GameObject player = Resources.Load("Player") as GameObject;
-		player = Network.Instantiate(player, initialLocation, initialRotation, 0) as GameObject;
-		m_leapManager.ConnectPlayer(player);
-		return player;
-	}
 
 	void OnConnectedToServer ()
 	{
-		m_playerIndex = 1;
 		Debug.Log("Connected to server");
-		m_players[m_playerIndex] = CreatePlayer(m_initialClientLocation, m_initialClientRotation);
-		m_networkView.RPC("ConfigurePlayer", RPCMode.AllBuffered, m_playerIndex);
+
+		m_playerIndex = 1;
+		NetworkViewID viewID = Network.AllocateViewID();
+		m_networkView.RPC("CreatePlayer", RPCMode.AllBuffered, m_playerIndex, viewID);
 	}
 }
