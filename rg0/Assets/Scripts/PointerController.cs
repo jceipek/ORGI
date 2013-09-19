7d66ec2c985489b346ef0af7064df62cf3865581
Julian Ceipek <julian.ceipek@gmail.com>
2013-09-19 18:11:37 -0400

    Removed old code.

--- rg0/Assets/Scripts/PointerController.cs
+++ rg0/Assets/Scripts/PointerController.cs
@@ -10,25 +10,6 @@ public class PointerController : MonoBehaviour
 	public float m_movementScale;
 	public Vector3 m_positionOffset;
 
-	private Camera m_camera;
-
-	void OnEnable ()
-	{
-		m_camera = Camera.main;
-	}
-
-	// Use this for initialization
-	void Start ()
-	{
-
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
-
-	}
-
 	// When the LeapManager detects that the leap has moved,
 	// it calls ToolMoved
 	public void ToolMoved (Vector3 position)
