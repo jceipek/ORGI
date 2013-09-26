f6726a3e1cd87828d82675554fe78e38e0192f30
Julian Ceipek <julian.ceipek@gmail.com>
2013-09-26 16:28:45 -0400

    Started RG1

---
+++ rg1/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+	public float m_projectionDistance;
+	private Camera m_camera;
+	private GameObject m_enemyPrefab;
+
+	void OnEnable ()
+	{
+		m_camera = Camera.main;
+		m_enemyPrefab = Resources.Load("Enemy") as GameObject;
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Ray spawnRay = m_camera.ScreenPointToRay(Input.mousePosition);
+			Vector3 spawnPoint =  spawnRay.origin + spawnRay.direction * m_projectionDistance;
+			Instantiate(m_enemyPrefab, spawnPoint, Quaternion.identity);
+		}
+	}
+
+	void OnDrawGizmos ()
+	{
+		if (!m_camera)
+		{
+			m_camera = Camera.main;
+		}
+		Gizmos.DrawWireCube(m_camera.transform.position +
+			                m_camera.transform.forward * m_projectionDistance,
+			                (Vector3.one + m_camera.transform.forward) * m_projectionDistance);
+	}
+
+}
