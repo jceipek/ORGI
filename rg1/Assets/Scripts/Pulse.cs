08a1be644ccdee372206d9d8c721f018e7285618
Cypress Frankenfeld <iamcypress@gmail.com>
2013-09-30 12:32:33 -0400

    added pulse script

---
+++ rg1/Assets/Scripts/Pulse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Pulse : MonoBehaviour {
+	private SerialPort stream = new SerialPort("/dev/tty.usbmodem411", 115200);
+	private string pulse_string;
+	private int pulse;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		stream.Open(); // Opens the serial port
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		pulse_string = stream.ReadLine(); // Reads the data from the arduino card
+		pulse = int.Parse(pulse_string);
+		Debug.Log(pulse);
+	}
+}
