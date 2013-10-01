3ccadf210106f4e18205da72e5a462e5d427cddc
Julian Ceipek <julian.ceipek@gmail.com>
2013-10-01 02:15:09 -0400

    Added bat movement

---
+++ rg1/Assets/BatAI.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatAI : MonoBehaviour {
+
+	public Transform m_leftBumper;
+	public Transform m_rightBumper;
+
+	public Vector3 m_direction;
+	public float m_speed;
+	public float m_rotSpeed;
+	public float m_rotationFactor;
+
+	void Start ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		MoveAvoid();
+		if (!AvoidRight())
+		{
+			AvoidLeft();
+		}
+	}
+
+	void MoveAvoid () {
+		float speedScale = GetMoveSpeed();
+		transform.position += (Time.fixedDeltaTime * speedScale * m_speed) * transform.forward;
+	}
+
+	bool AvoidRight ()
+	{
+		RaycastHit hitInfo;
+		bool avoid = Physics.Raycast(transform.position, m_rightBumper.position-transform.position, out hitInfo);
+		if (avoid)
+		{
+			float rotationSlowFactor = 0.0f;
+			if (hitInfo.distance != 0.0f) {
+				rotationSlowFactor = Mathf.Exp(m_rotationFactor * -hitInfo.distance);
+			}
+			transform.Rotate(transform.up * rotationSlowFactor * -m_rotSpeed * Time.fixedDeltaTime);
+		}
+		return avoid;
+	}
+
+	bool AvoidLeft ()
+	{
+		RaycastHit hitInfo;
+		bool avoid = Physics.Raycast(transform.position, m_leftBumper.position-transform.position, out hitInfo);
+		if (avoid)
+		{
+			float rotationSlowFactor = 0.0f;
+			if (hitInfo.distance != 0.0f) {
+				rotationSlowFactor = Mathf.Exp(m_rotationFactor * -hitInfo.distance);
+			}
+			transform.Rotate(transform.up * rotationSlowFactor * m_rotSpeed * Time.fixedDeltaTime);
+		}
+		return avoid;
+	}
+
+	float GetMoveSpeed ()
+	{
+		float scale = 1.0f;
+		RaycastHit hitInfo;
+		if (Physics.Raycast(transform.position, transform.forward, out hitInfo))
+		{
+			scale =  Mathf.Lerp(0.0f, 1.0f, 1.0f - Mathf.Exp(-hitInfo.distance));
+		}
+		return scale;
+	}
+
+	void OnDrawGizmos ()
+	{
+		Gizmos.DrawLine(transform.position,transform.position+transform.forward * m_speed);
+		Gizmos.DrawLine(transform.position,m_leftBumper.position);
+		Gizmos.DrawLine(transform.position,m_rightBumper.position);
+	}
+}
--- rg1/Assets/Scripts/BatGenerator.cs
+++ rg1/Assets/Scripts/BatGenerator.cs
@@ -9,44 +9,6 @@ public class BatGenerator : MonoBehaviour {
 	private MeshRenderer m_meshRenderer;
 	private int m_triangleCount = 10;
 	private float m_size = 1.5f;
-/*
-	void Start ()
-	{
-		m_meshFilter = gameObject.GetComponent<MeshFilter>();
-
-		Mesh mesh = new Mesh();
-		mesh.name = "testMesh";
-		mesh.Clear();
-		Vector3[] vertices = new Vector3[4];
-		vertices[0] = new Vector3(-10.0f, 0.0f, 0.0f);
-		vertices[1] = new Vector3(10.0f, 0.0f, 0.0f);
-		vertices[2] = new Vector3(-10.0f, 50.0f, 0.0f);
-		vertices[3] = new Vector3( 10.0f, 50.0f, 0.0f);
-		mesh.vertices = vertices;
-
-		Vector2[] uv = new Vector2[4];
-		uv[0] = new Vector2(0,0);
-		uv[1] = new Vector2(1,0);
-		uv[2] = new Vector2(0,1);
-		uv[3] = new Vector2(1,1);
-		mesh.uv = uv;
-
-		int[] triangles = new int[6];
-		triangles[0] = 0;
-		triangles[1] = 1;
-		triangles[2] = 2;
-		triangles[3] = 1;
-		triangles[4] = 3;
-		triangles[5] = 2;
-		mesh.triangles = triangles;
-		mesh.RecalculateNormals();
-
-		MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
-		m_meshFilter.mesh = mesh;
-		mr.renderer.material.color = Color.white;
-	}
-*/
-
 
 	void Start ()
 	{
@@ -78,7 +40,6 @@ public class BatGenerator : MonoBehaviour {
 	{
 		Mesh mesh = m_meshFilter.mesh;
 		Vector3[] vertices = mesh.vertices;
-		//Vector3[] normals = mesh.normals;
 		for (int i = 0; i < m_triangleCount * 3; i++) {
 			vertices[i] = Random.insideUnitSphere * m_size;
 		}
