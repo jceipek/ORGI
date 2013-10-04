98cb15a8068b44ad43717a04a946ed3803484205
Julian Ceipek <julian.ceipek@gmail.com>
2013-10-04 16:45:17 -0400

    Tracking avatar motion

--- rg1/Assets/Scripts/AvatarNetworking.cs
+++ rg1/Assets/Scripts/AvatarNetworking.cs
@@ -5,18 +5,36 @@ public class AvatarNetworking : MonoBehaviour {
 
 	public Vector3 m_playerPosition;
 	public Quaternion m_playerRotation;
+	private NetworkView m_networkView;
 
-	void OnEnable ()
+	void OnConnectedToServer ()
 	{
-		//if (Server.g) Server.g.ConnectAvatar(GetComponent<NetworkView>());
+		if (Server.g && Server.g.IsClient()) {
+			m_networkView = GetComponent<NetworkView>();
+			Server.g.SyncViewIds (m_networkView, "Avatar");
+		}
 	}
 	
 	void Update ()
 	{
-		// if (Server.g && Server.g.IsClient())
-		// {
-		// 	m_playerPosition = transform.position;
-		// 	m_playerRotation = transform.rotation;
-		// }
+		if (Server.g)
+		{
+			if (Server.g.IsServer())
+			{
+				transform.position = m_playerPosition;
+				transform.rotation = m_playerRotation;	
+			}
+			else
+			{
+				m_networkView.RPC("SyncTransform", RPCMode.Others, ((Transform)m_networkView.observed).rotation, ((Transform)m_networkView.observed).position);
+			}
+			
+		}
+	}
+
+	[RPC]
+	public void SyncTransform(Quaternion rotation, Vector3 position) {
+		m_playerPosition = position;
+		m_playerRotation = rotation;
 	}
 }
